299d2fa3f952a4603bd5e8cd9752824953de277c
Colin <Colin.Reardon@triosstudent.com>
2024-05-06 21:27:16 -0400

    Added new font and game over screen

--- Pong1/SDL_Template/Scoreboard.cpp
+++ Pong1/SDL_Template/Scoreboard.cpp
@@ -17,10 +17,11 @@ void Scoreboard::Score(int score) {
 	ClearBoard();
 
 	if (score == 0) {
-		for (int i = 0; i < 2; i++) {
-			mScore.push_back(new GLTexture("0", "emulogic.ttf", 32, mColor));
+		for (int i = 0; i < 1; i++) {
+			mScore.push_back(new GLTexture("0", "Pixeled.ttf", 32, mColor));
 			mScore[i]->Parent(this);
-			mScore[i]->Position(Vector2(-32.0f * i, 0.0f));
+			mScore[i]->Position(Vector2(-55.0f * i, 0.0f));
+			mScore[i]->Scale(Vector2(1.7f, 2.50f));
 		}
 	}
 	else {
@@ -28,9 +29,10 @@ void Scoreboard::Score(int score) {
 		unsigned lastIndex = (unsigned)str.length() - 1;
 
 		for (unsigned i = 0; i <= lastIndex; i++) {
-			mScore.push_back(new GLTexture(str.substr(i, 1), "emulogic.ttf", 32, mColor));
+			mScore.push_back(new GLTexture(str.substr(i, 1), "Pixeled.ttf", 32, mColor));
 			mScore[i]->Parent(this);
-			mScore[i]->Position(Vector2(-32.0f * (lastIndex - i), 0.0f));
+			mScore[i]->Position(Vector2(-55.0f * (lastIndex - i), 0.0f));
+			mScore[i]->Scale(Vector2(1.7f, 2.50f));
 		}
 	}
 }
